934762e03b661bea3961cb9116ba633e789faf3d
mhhu <mhhu@liangzhicn.com>
2022-09-15 19:41:40 +0800

    update

---
+++ src/main/java/skily_leyu/mistyrain/block/MRRenderType.java
@@ -0,0 +1,17 @@
+package skily_leyu.mistyrain.block;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD,value = Dist.CLIENT)
+public class MRRenderType {
+
+    @SubscribeEvent
+    public static void onRenderTypeSetup(FMLClientSetupEvent event){
+        RenderTypeLookup.setRenderLayer(MRBlock.blockSnowVelvet.get(), RenderType.cutout());
+    }
+}
--- src/main/java/skily_leyu/mistyrain/block/potplant/BlockSnowVelvet.java
+++ src/main/java/skily_leyu/mistyrain/block/potplant/BlockSnowVelvet.java
@@ -6,14 +6,21 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 import skily_leyu.mistyrain.config.MRSetting;
 
 public class BlockSnowVelvet extends Block{
 
     private static IntegerProperty STAGE = MRSetting.PLANT_STAGE_HEX;
+    private static VoxelShape SHAPE = VoxelShapes.create(new AxisAlignedBB(0, 0, 0, 1.0, 1.0, 1.0));
 
     public BlockSnowVelvet() {
-        super(AbstractBlock.Properties.of(Material.PLANT));
+        super(AbstractBlock.Properties.of(Material.PLANT).noCollission());
         this.registerDefaultState(this.defaultBlockState().setValue(STAGE, 0));
         this.defaultBlockState().setValue(STAGE, 0);
     }
@@ -22,6 +29,12 @@ public class BlockSnowVelvet extends Block{
     protected void createBlockStateDefinition(Builder<Block, BlockState> container) {
         container.add(STAGE);
         super.createBlockStateDefinition(container);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_220053_1_, IBlockReader p_220053_2_, BlockPos p_220053_3_,
+            ISelectionContext p_220053_4_) {
+        return SHAPE;
     }
 
 }
