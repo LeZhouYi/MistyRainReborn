49264b9b33fabfc91eaee575040b70bb33be2e80
mhhu <mhhu@liangzhicn.com>
2023-11-13 21:12:11 +0800

    add chapterButton

--- src/main/java/skily_leyu/mistyrain/common/core/book/Chapter.java
+++ src/main/java/skily_leyu/mistyrain/common/core/book/Chapter.java
@@ -1,7 +1,13 @@
 package skily_leyu.mistyrain.common.core.book;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class Chapter {
 
+    private String key; //关键字
     private String name; //章名
     private String icon; //图标路径
     private String parentNode; //所属目录
@@ -22,4 +28,20 @@ public class Chapter {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public boolean isRoot(){
+        return parentNode == null;
+    }
+
+    public TranslationTextComponent getName(){
+        return new TranslationTextComponent(this.key+"."+this.name);
+    }
+
+    public ItemStack getItemstack(){
+        if(this.name.isEmpty()){
+            return ItemStack.EMPTY;
+        }
+        return new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(icon)));
+    }
+
 }
